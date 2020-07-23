75b4fb97617947e30fd85170d0668ee052fa42a3
ss-hoon <46196555+ss-hoon@users.noreply.github.com>
2020-07-23 23:02:29 +0900

    스택 구현 - 배열(자바)

--- 스택/StackArray.java
+++ 스택/StackArray.java
@@ -79,13 +79,4 @@ public class StackArray{
         System.out.println(st.pop());
         st.peek();
     }
-}{
-    public static void main(String[] args){
-        Stack st = new Stack(100);
-        st.push(1);
-        st.push(2);
-        st.peek();
-        System.out.println(st.pop());
-        st.peek();
-    }
 }
