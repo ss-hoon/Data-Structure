34b4266d2515902ed48e8f1780b7501870d6bb69
ss-hoon <46196555+ss-hoon@users.noreply.github.com>
2021-01-08 14:50:51 +0900

    스택 구현 - 배열(자바)
    중복 메소드 삭제

--- 스택/StackArray.java
+++ 스택/StackArray.java
@@ -34,42 +34,6 @@ class Stack{
         }
     }
 }
-public class class Stack{
-    int top; // 스택의 top
-    int[] stack; // stack 배열
-    int stack_maxSize; // stack 최대 크기
-
-    Stack(int size){ // Stack 생성자
-        top = -1;
-        stack = new int[size];
-        stack_maxSize = size;
-    }
-
-    public void push(int data){ // push 메소드
-        if(top >= stack_maxSize - 1){
-            System.out.println("Stack is Full");
-        } else {
-            stack[++top] = data;
-        }
-    }
-
-    public int pop(){ // pop 메소드
-        if(top == -1){
-            System.out.println("Stack is Empty");
-            return 0;
-        } else {
-            return stack[top--];
-        }
-    }
-
-    public void peek(){ // peek 메소드
-        if(top == -1){
-            System.out.println("Stack is Empty");
-        } else {
-            System.out.println(stack[top]);
-        }
-    }
-}
 public class StackArray{
     public static void main(String[] args){
         Stack st = new Stack(100);
